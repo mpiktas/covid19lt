58d750eedf3ded20ab78ae7acb9df088306a0889
Vaidotas Zemlys <zemlys@gmail.com>
2020-03-21 13:47:37 +0200

    Add tested data plus simple statistics

--- R/combine.R
+++ R/combine.R
@@ -1,8 +1,9 @@
 library(lubridate)
 library(dplyr)
+library(tidyr)
 
-fns <- dir("total", pattern="[0-9]+.csv", full.names = TRUE)
+fns <- dir("total", pattern = "[0-9]+.csv", full.names = TRUE)
 
-dt <- fns %>% lapply(read.csv, stringsAsFactor=FALSE) %>%
-    bind_rows %>% arrange(country,day) %>%
+dt <- fns %>% lapply(read.csv, stringsAsFactor = FALSE) %>%
+    bind_rows %>% arrange(country,day) %>% fill(under_observation) %>%
     write.csv("total/lt-covid19-total.csv", row.names = FALSE)
---
+++ stats.md
@@ -0,0 +1,10 @@
+COVID-19 statistics
+================
+
+![](stats_files/figure-gfm/unnamed-chunk-2-1.png)<!-- -->
+
+![](stats_files/figure-gfm/unnamed-chunk-3-1.png)<!-- -->
+
+![](stats_files/figure-gfm/unnamed-chunk-4-1.png)<!-- -->
+
+![](stats_files/figure-gfm/unnamed-chunk-5-1.png)<!-- -->
