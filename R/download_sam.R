009dcdc22666c8177657c1c8c1f8bb202c9e07b1
Vaidotas Zemlys <zemlys@gmail.com>
2020-04-06 12:44:17 +0300

    Add the new tests data

--- R/download_sam.R
+++ R/download_sam.R
@@ -11,14 +11,7 @@ oo <- read_html(raw)
 cd <- html_nodes(oo,".text") %>% html_nodes("li") %>% html_text
 tbs <- html_table(oo, fill = TRUE)
 
-if(FALSE) {
-
-cdd <- cd %>% strsplit(":")
-cdd <- cdd[sapply(cdd, length) == 2]
-nums <- cdd %>% sapply("[[", 2) %>% gsub("(.{1})([0-9]*)","\\2",.) %>% as.integer
-
-
-fns <- dir("total", pattern="[0-9]+.csv", full.names  = TRUE)
+fns <- dir("tests", pattern="[0-9]+.csv", full.names  = TRUE)
 
 days <- fns %>% strsplit("-") %>% sapply(function(x)gsub(".csv","",x[length(x)]))
 
@@ -29,6 +22,32 @@ new_day <- max(daysd)+days(1)
 outd <- gsub("-","",as.character(new_day))
 
 new_day_data <- read.csv(fns[which.max(daysd)], stringsAsFactors = FALSE)
+
+tb1 <- tbs[[1]][-1:-4,]
+
+colnames(tb1) <- c("laboratory", "tested_all", "tested_mobile", "negative_all", "negative_mobile", "positive_all","positive_mobile", "not_tested", "not_tested_mobile")
+
+tb1[, -1] <- sapply(tb1[, -1], as.integer)
+
+tbr <- tb1 %>% filter(laboratory != "VISO")
+
+tbr <- bind_cols(data.frame(day = rep(outd, nrow(tbr))), tbr)
+
+tot <- tbr[,-1:-2] %>% sapply(sum, na.rm = TRUE)
+
+if ( sum(abs(tot - tb1 %>% filter(laboratory == "VISO") %>% .[,-1] %>% unlist)) == 0) {
+    write.csv(tbr, glue::glue("tests/lt-covid19-laboratory-{outd}.csv"), row.names = FALSE )
+}
+
+
+
+if(FALSE) {
+
+cdd <- cd %>% strsplit(":")
+cdd <- cdd[sapply(cdd, length) == 2]
+nums <- cdd %>% sapply("[[", 2) %>% gsub("(.{1})([0-9]*)","\\2",.) %>% as.integer
+
+
 new_day_data$day <- new_day
 
 if ((nums[1] - nums[2]) == new_day_data$confirmed[1]) new_day_data$confirmed[1] <- nums[1]
@@ -41,19 +60,4 @@ if ((nums[6] - nums[5]) == new_day_data$tested[1]) new_data_day$tested[1] <- num
 
 write.csv(new_day_data, glue::glue("total/lt-covid19-total-{outd}.csv"), row.names = FALSE )
 
-}
-tb1 <- tbs[[1]][-1:-4,]
-
-colnames(tb1) <- c("laboratory", "tested_all", "tested_mobile", "negative_all", "negative_mobile", "positive_all","positive_mobile", "not_tested", "not_tested_mobile")
-
-tb1[, -1] <- sapply(tb1[, -1], as.integer)
-
-tbr <- tb1 %>% filter(laboratory != "VISO")
-
-tbr <- bind_cols(data.frame(day = rep(outd, nrow(tbr))), tbr)
-
-tot <- tbr[,-1] %>% sapply(sum, na.rm = TRUE)
-
-if ( sum(abs(tot - tb1 %>% filter(laboratory == "VISO") %>% .[,-1] %>% unlist)) == 0) {
-    write.csv(tbr, glue::glue("tests/lt-covid19-laboratory-{outd}.csv"), row.names = FALSE )
 }
