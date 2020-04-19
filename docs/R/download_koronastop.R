7bf77e9d1cff0532afee2b9504d80b08bc61c8e8
Vaidotas Zemlys <zemlys@gmail.com>
2020-04-19 17:25:17 +0300

    Updat the stats

--- docs/R/download_koronastop.R
+++ docs/R/download_koronastop.R
@@ -56,6 +56,6 @@ if ((nums[6] - nums[5]) == new_day_data$tested[1]) {
 write.csv(new_day_data, glue::glue("total/lt-covid19-total-{outd}.csv"), row.names = FALSE )
 
 ndd <- new_day_data %>% select(country, day) %>% mutate(incidence = nums[2], daily_tests = nums[5])
-write.csv(new_day_data, glue::glue("daily/lt-covid19-daily-{outd}.csv"), row.names = FALSE )
+write.csv(ndd, glue::glue("daily/lt-covid19-daily-{outd}.csv"), row.names = FALSE )
 
 
