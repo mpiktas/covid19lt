e0ed4a52288e7ee946c4d239528fd3885088cd3c
Vaidotas Zemlys <zemlys@gmail.com>
2020-04-28 18:40:49 +0300

    Adapt the code for the new data and download it

--- R/download_tests_table.R
+++ R/download_tests_table.R
@@ -29,7 +29,7 @@ new_day_data <- read.csv(fns[which.max(daysd)], stringsAsFactors = FALSE)
 
 tb1 <- tbs[[1]][-1:-4,]
 
-colnames(tb1) <- c("laboratory", "tested_all", "tested_mobile", "negative_all", "negative_mobile", "positive_new","positive_mobile","positive_retested","not_tested", "not_tested_mobile")
+colnames(tb1) <- c("laboratory", "tested_all", "tested_mobile", "negative_all", "negative_mobile", "positive_all","positive_mobile","not_tested", "not_tested_mobile")
 
 tb1[, -1] <- sapply(tb1[, -1], function(x)as.integer(gsub("*","",x, fixed = TRUE)))
 
@@ -40,7 +40,7 @@ tbr <- bind_cols(data.frame(day = rep(new_day, nrow(tbr))), tbr)
 tot <- tbr[,-1:-2] %>% sapply(sum, na.rm = TRUE)
 if(sum(abs(tot - tb1 %>% filter(laboratory == "VISO") %>% .[,-1] %>% unlist)) != 0) warning("Totals do not match")
 
-tbr <- tbr %>% mutate(positive_all = positive_new+positive_retested) %>%
+tbr <- tbr %>% mutate(positive_new = NA, positive_retested = NA) %>%
     select(day, laboratory, tested_all, tested_mobile,
            negative_all, negative_mobile,
            positive_all, positive_mobile, positive_new, positive_retested,
@@ -48,10 +48,13 @@ tbr <- tbr %>% mutate(positive_all = positive_new+positive_retested) %>%
 
 
 ##Compare with the previous days data:
+if(identical(dim(tbr),dim(new_day_data))) {
+    sm <- sum(abs(new_day_data[,-2:-1]-tbr[,-2:-1]), na.rm=TRUE)
+}else {
+    sm <- 0
+}
 
-sm <- sum(abs(new_day_data[,-2:-1]-tbr[,-2:-1]), na.rm=TRUE)
-
-if (sm > 0) {
+if (sm == 0) {
     write.csv(tbr, glue::glue("tests/lt-covid19-laboratory-{outd}.csv"), row.names = FALSE )
 } else {
     warning("New day data is identical to the previous day")
