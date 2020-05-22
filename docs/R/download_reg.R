c66ee80caaffd92b92ad2a28aa635c06c67ebbe5
Vaidotas Zemlys <zemlys@gmail.com>
2020-05-22 13:52:22 +0300

    Update the site

--- docs/R/download_reg.R
+++ docs/R/download_reg.R
@@ -10,9 +10,20 @@ ds <- GET("https://maps.registrucentras.lt/arcgis/rest/services/covid/pjuviai/Ma
 geo <- GET("https://maps.registrucentras.lt/arcgis/rest/services/covid/pjuviai/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=MIRE%20desc&resultType=standard&callback=dojo_request_script_callbacks.dojo_request_script24")
 
 
-dst <- rawToChar(ds$content)
 
-dst1 <- fromJSON(sub("[)];$","",sub("^.*[(]","",dst)))
 
-geo1 <- fromJSON(sub("[)];$","",sub("^.*[(]","",geo)))
+dst1 <- fromJSON(sub("[)];$","",sub("^.*[(]","",rawToChar(ds$content))))
+
+geo1 <- fromJSON(sub("[)];$","",sub("^.*[(]","",gsub("(\"ATNUJINTA\":)([0-9]+)","\\1\"\\2\"", rawToChar(geo$content)))))
+
+
+
+dd <- gsub(" ","_",Sys.time())
+
+write.csv(dst1$features$attributes, file = paste0("rc/susirgimai_", dd,".csv"), row.names=FALSE)
+
+write.csv(geo1$features$attributes, file = paste0("rc/savivaldybes_", dd,".csv"), row.names=FALSE)
+
+
+
 
