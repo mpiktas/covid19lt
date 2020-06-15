fc0a24d021c6ef80912d553417b6de63fbc15c88
Vaidotas Zemlys <zemlys@gmail.com>
2020-06-15 08:31:40 +0300

    Update the site

--- R/normalize_regions.R
+++ R/normalize_regions.R
@@ -57,4 +57,34 @@ svd2 <-  svd1 %>% group_by(day) %>%
            updated =ATNUJINTA
     )
 
+# Do age groups -----------------------------------------------------------
+
+fn <- dir("rc", full.names = TRUE)
+
+fns <- c(fn[grepl("amziaus_grupes_",fn)])
+
+ag <- lapply(fns, read.csv, stringsAsFactors = FALSE)
+
+pt <- strsplit(fns, "_") %>% lapply(function(x)ymd_hms(paste(x[3:4],collapse="_")))
+
+ag1 <- mapply(function(dt, tm) dt %>% mutate(downloaded = tm), ag, pt, SIMPLIFY = FALSE) %>% bind_rows %>%
+    select(-X) %>% mutate(day = ymd(floor_date(downloaded, unit = "day")) - days(1))
+
+ag2 <-  ag1 %>% group_by(day) %>%
+    filter(downloaded == max(downloaded)) %>% ungroup %>%
+    select(day, age_group= AMZIAUSGRUPE,
+           confirmed = TEIGIAMI,
+           confirmed_100k = ATVEJAI_VISI_100000,
+           confirmed_male = VYRAI,
+           confirmed_male_100k = ATVEJAI_V_100000,
+           confirmed_female  = MOTERYS,
+           confirmed_female_100k  = ATVEJAI_M_100000,
+           other = KITA,
+           total = VISO,
+           population = GYV_SKAIC,
+           population_male = GYV_SKAIC_V,
+           population_female = GYV_SKAIC_M,
+           downloaded
+    )
+
 
