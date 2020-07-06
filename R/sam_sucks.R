89aa985e35d2780186b8ea7a42982938577aeaaf
Vaidotas Zemlys <zemlys@gmail.com>
2020-07-06 22:23:40 +0300

    SAM sucks, so when it does not renew data, it is written down by hand.

---
+++ R/sam_sucks.R
@@ -0,0 +1,67 @@
+fns <- dir("total", pattern="[0-9]+.csv", full.names  = TRUE)
+
+days <- fns %>% strsplit("-") %>% sapply(function(x)gsub(".csv","",x[length(x)]))
+
+daysd <- ymd(days)
+
+new_day <- max(daysd)+days(1)
+
+outd <- gsub("-","",as.character(new_day))
+
+new_day_data <- read.csv(fns[which.max(daysd)], stringsAsFactors = FALSE)
+
+old_day_data <- new_day_data
+
+new_day_data$day <- new_day
+
+##1. confirmed
+##2. active
+##3. incidence
+##4. deaths
+##5. deaths for other reasons
+##6. recovered
+##7. tests
+##8. total tests
+nums <- c(1841, 204, 5, 79, 11, 1547, 70, 1149, 443391)
+ia1 <- 37
+
+
+if ((nums[1] - nums[3]) == new_day_data$confirmed[1])  {
+    new_day_data$confirmed[1] <- nums[1]
+} else {
+    warning("Confirmed numbers do not match")
+    new_day_data$confirmed[1] <-  new_day_data$confirmed[1] + nums[3]
+}
+
+if (nums[4] >= new_day_data$deaths[1]) {
+    new_day_data$deaths[1] <- nums[4]
+}else warning("Deaths number is lower")
+
+if (nums[6] >= new_day_data$recovered[1]) {
+    new_day_data$recovered[1] <- nums[6]
+} else warning("Recovered number is lower")
+
+if ((nums[9] - nums[8]) == new_day_data$tested[1])  {
+    new_day_data$tested[1] <- nums[9]
+} else  {
+    warning("Tested numbers do not match")
+    new_day_data$tested[1] <- nums[9]
+}
+
+new_day_data$quarantined[1] <- nums[7]
+
+write.csv(new_day_data, glue::glue("total/lt-covid19-total-{outd}.csv"), row.names = FALSE )
+
+ndd <- new_day_data %>% select(country, day) %>%
+    mutate(confirmed = nums[1],
+           active = nums[2],
+           incidence = nums[3],
+           deaths = nums[4],
+           deaths_different =nums[5],
+           recovered = nums[6],
+           daily_tests = nums[8],
+           quarantined = nums[7],
+           total_tests = nums[9],
+           imported0601 = ia1)
+write.csv(ndd, glue::glue("daily/lt-covid19-daily-{outd}.csv"), row.names = FALSE )
+
