f0e5c2244c47acd07d0d58010f0dc7bc1fd191d5
Vaidotas Zemlys <zemlys@gmail.com>
2020-08-25 22:02:25 +0300

    Save csv, also save the backup into local folder

--- R/individual.R
+++ R/individual.R
@@ -2,7 +2,12 @@ library(jsonlite)
 
 system("wget ftp://atviriduomenys.nvsc.lt/COVID19.json")
 file.copy("COVID19.json","individual/COVID19.json")
+suff <- gsub("-","",Sys.Date())
+
+file.copy("COVID19.json",paste0("~/tmp/covid19lt-json/COVID19-",suff,".json"))
 file.remove("COVID19.json")
+
+
 
 zz <- fromJSON("individual/COVID19.json")
 
@@ -14,10 +19,14 @@ zz1 <- zz %>% rename(actual_day = `Susirgimo data`,
                      foreigner  = `Užsienietis`,
                      age  =  `Atvejo amžius`,
                      sex = `Lytis`,
-                     administrative_level3 = `Savivaldybė`,
+                     administrative_level_3 = `Savivaldybė`,
                      hospitalized = `Ar hospitalizuotas`,
                      intensive = `Gydomas intensyvioje terapijoje`,
-                     precondition = `Turi lėtinių ligų`)
+                     precondition = `Turi lėtinių ligų`) %>%
+    arrange(day, actual_day, administrative_level_3)
+
+zz1 %>% write.csv("individual/lt-covid19-individual.csv", row.names = FALSE)
+
 
 zz2 <- lapply(sort(unique(zz1$day)), function(d) zz1 %>% filter(day <= d))
 
