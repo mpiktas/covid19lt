65240b1ec21ddf02985e7757f7412adc08c6af20
Vaidotas Zemlys <zemlys@gmail.com>
2020-09-25 22:40:19 +0300

    Add age-region death distribution.

--- R/individual.R
+++ R/individual.R
@@ -91,31 +91,47 @@ if(FALSE) {
 
 
 
-daily_xtable <- function(zz1, ztotal = FALSE) {
+daily_xtable <- function(zz1, colsums = FALSE) {
 
     agad <- zz1 %>% count(day, administrative_level_3, age)  %>%
         pivot_wider(names_from = "age", values_from ="n", values_fill = 0,names_sort = TRUE) %>%
         arrange(day, administrative_level_3)
 
-    ag <- zz1 %>% count(day, age)
-
-    ag1 <- ag %>% group_by(day) %>% summarise(n = sum(n))
-    ag2 <- ag %>% bind_rows(ag1 %>% mutate(age = "Total")) %>%
-        pivot_wider(names_from = "age", values_from ="n", values_fill = 0,names_sort = TRUE) %>%
-        mutate(administrative_level_3 = "ZTotal")
+    agr <- zz1$age %>% unique
+    sagr <- sapply(strsplit(agr, "-"),"[[",1) %>% as.integer
+    agr_sorted <- agr[order(sagr)]
 
     ad <- zz1 %>% count(day,administrative_level_3) %>% rename(Total = n)
 
     agad1 <- agad %>% inner_join(ad)
-    agad2 <- agad1 %>% bind_rows(ag2) %>%
-        select(day, administrative_level_3, `0-9`, `10-19`, `20-29`, `30-39`, `40-49`, `50-59`, `60-69`,`70-79`,`80-89`,`90-99`, `100-109`, Total) %>%
-        arrange(day, administrative_level_3)
-    agad2
+
+    if(colsums) {
+        ag <- zz1 %>% count(day, age)
+        ag1 <- ag %>% group_by(day) %>% summarise(n = sum(n))
+        ag2 <- ag %>% bind_rows(ag1 %>% mutate(age = "Total")) %>%
+            pivot_wider(names_from = "age", values_from ="n", values_fill = 0,names_sort = TRUE) %>%
+            mutate(administrative_level_3 = "ZTotal")
+
+        agad2 <- agad1 %>% bind_rows(ag2) %>%
+            arrange(day, administrative_level_3) %>%
+            mutate(administrative_level_3 = gsub("ZTotal","Total", administrative_level_3))
+    } else {
+        agad2 <- agad1 %>% arrange(day, administrative_level_3)
+    }
+
+    agad2[, c("day","administrative_level_3", agr_sorted, "Total")]
 }
+
+
+daily_xtable(zz1, colsums = TRUE) %>% write.csv("data/lt-covid19-age-region-incidence.csv", row.names =  FALSE)
+
+daily_xtable(zz1 %>% filter(status = "Mirė")) %>% write.csv("data/lt-covid19-age-region-deaths.csv", row.names =  FALSE)
 
 #agad2 %>% write.csv("data/lt-covid19-age-region-incidence.csv", row.names = FALSE)
 #
 
-ad <- zz1 %>% count(day,administrative_level_3) %>% mutate(day = ymd(day))
+#ad <- zz1 %>% count(day,administrative_level_3) %>% mutate(day = ymd(day))
 
-ggplot(aes(x= ))
+#ggplot(aes(x=day, y = n ),
+#       data = ad %>% filter(day >= "2020-07-01", administrative_level_3 %in% (agad2 %>% filter(day == max(day)) %>% pull(administrative_level_3)))) +
+#           facet_wrap(~administrative_level_3, scales = "free_y") +geom_line()
