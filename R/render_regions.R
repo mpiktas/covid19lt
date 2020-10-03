9f21f0d74f0e3363024737433e63283a17ecade1
Vaidotas Zemlys <zemlys@gmail.com>
2020-10-03 16:41:34 +0300

    Add the index for regions

--- R/render_regions.R
+++ R/render_regions.R
@@ -4,4 +4,7 @@ regs <- read.csv("data/lt-covid19-individual.csv") %>% .$administrative_level_3 
 
 regs_n <- paste0(gsub("[.]","",gsub(" ", "_",regs)),".html")
 
-out <- mapply(function(r, f) rmarkdown::render("R/region.Rmd", output_dir = 'docs/regions/', output_file = f, params = list(region = r)), regs, regs_n)
+out <- mapply(function(r, f) try(rmarkdown::render("R/region.Rmd", output_dir = 'docs/regions/', output_file = f, params = list(region = r))),
+              regs, regs_n, SIMPLIFY  = FALSE)
+
+rmarkdown::render("R/regions_index.Rmd", output_dir = 'docs/', output_file = "savivaldybes.html")
