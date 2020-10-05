59870964db22220240cc34094c64756c1e77c7f7
Vaidotas Zemlys <zemlys@gmail.com>
2020-10-05 22:57:34 +0300

    Add hospitalization data parsing code and results

---
+++ R/download_sam_tables.R
@@ -0,0 +1,55 @@
+library(httr)
+library(rvest)
+library(jsonlite)
+library(dplyr)
+library(lubridate)
+
+raw <- GET("http://sam.lrv.lt/lt/naujienos/koronavirusas")
+#writeLines(unlist(strsplit(gsub("\n+","\n",gsub("(\n )+","\n",gsub(" +"," ",gsub("\r|\t", "", html_text(read_html(raw)))))),"\n")), paste0("/home/vaidotas/R/corona/data/korona_LT_",gsub( ":| ","_",raw$date),".csv"))
+
+oo <- read_html(raw)
+
+tbs <- html_table(oo, fill = TRUE)
+
+# Get the total capacity data ------------------------------------------------------
+
+
+capacity_total <- tbs[[1]][-2:-1,]
+colnames(capacity_total) <- c("description", "total", "intensive", "ventilated", "oxygen_mask")
+capacity_total[,-1] <- sapply(capacity_total[,-1], as.integer)
+rownames(capacity_total) <- NULL
+
+
+# Get covid hospitalisation data ----------------------------------------------------------
+
+cvh <- tbs[[2]][-2:-1,]
+
+colnames(cvh) <- c("description","total", "oxygen","ventilated","hospitalized_not_intensive", "intensive")
+cvh[,-1] <- sapply(cvh[,-1], as.integer)
+
+
+# Get regional hospitalization data ---------------------------------------
+
+tlk <- tbs[[3]][-2:-1,]
+colnames(tlk) <-c("description", "tlk", "total", "intensive", "ventilated", "oxygen_mask")
+tlk[,-2:-1] <- sapply(tlk[,-2:-1], function(x)as.integer(gsub("[,.]","",x)))
+
+tt <- tlk %>% filter(tlk == "Iš viso:")
+
+tlk <- tlk %>% filter(tlk != "Iš viso:")
+
+tt1 <- tlk %>% select(-tlk) %>% group_by(description) %>% summarise_all(sum)
+
+test_total <- sum(tt[order(tt$description), -1:-2] - tt1[order(tt1$description),-1])
+if(test_total != 0) warning("Totals do not match with TLK breakdown")
+
+
+# Write everything --------------------------------------------------------
+
+res <- list(total_capacity= capacity_total, covid_hospitalization = cvh, tlk_capacity = tlk)
+
+dd <- gsub(" ","_",Sys.time())
+fnl <- paste0("raw_data/hospitalization//",names(res),"_",dd,".csv")
+
+mapply(function(dt, nm) write.csv(dt, nm, row.names = FALSE), res, fnl, SIMPLIFY = FALSE)
+
---
+++ R/normalize_hospitalization.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(lubridate)
+
+fn <- dir("raw_data/hospitalization", full.names = TRUE)
+
+fns <- c(fn[grepl("tlk_",fn)])
+
+tlk <- lapply(fns, read.csv, stringsAsFactors = FALSE)
+
+pt <- strsplit(fns, "_") %>% lapply(function(x)ymd_hms(paste(x[4:5],collapse="_")))
+
+tlk1 <- mapply(function(dt, tm) dt %>% mutate(downloaded = tm), tlk, pt, SIMPLIFY = FALSE) %>%
+    bind_rows %>%  mutate(day = ymd(floor_date(downloaded, unit = "day")) - days(1))
+
+tlk2 <-  tlk1 %>% group_by(day) %>%
+    filter(downloaded == max(downloaded)) %>% ungroup %>%
+    select(day, description, tlk, total, intensive, ventilated, oxygen_mask, downloaded)
+
+write.csv(tlk2, "data/hospital-capacity.csv", row.names = FALSE)
+
+# Do the same for another layer -------------------------------------------
+
+fn <- dir("raw_data/hospitalization", full.names = TRUE)
+
+fns <- c(fn[grepl("covid_",fn)])
+
+cvh <- lapply(fns, read.csv, stringsAsFactors = FALSE)
+
+pt <- strsplit(fns, "_") %>% lapply(function(x)ymd_hms(paste(x[4:5],collapse="_")))
+
+cvh1 <- mapply(function(dt, tm) dt %>% mutate(downloaded = tm), cvh, pt, SIMPLIFY = FALSE) %>% bind_rows %>%
+     mutate(day = ymd(floor_date(downloaded, unit = "day")) - days(1))
+
+cvh2 <-  cvh1 %>% group_by(day) %>%
+    filter(downloaded == max(downloaded)) %>% ungroup %>%
+    select(day, total, oxygen_mask = oxygen,
+           ventilated,
+           intensive, downloaded)
+
+write.csv(cvh2, "data/lt-covid19-hospitalized.csv", row.names = FALSE)
+
