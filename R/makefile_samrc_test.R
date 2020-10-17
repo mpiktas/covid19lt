13c14e3691c82c52c3c9de7bc2aecb3aa593d690
Vaidotas Zemlys <zemlys@gmail.com>
2020-10-17 18:19:41 +0300

    More CI tests

--- R/makefile_samrc_test.R
+++ R/makefile_samrc_test.R
@@ -19,9 +19,13 @@ modf <- git_status() %>% .$file %>% find_root %>% unique
 
     ghpt <- Sys.getenv("GITHUB_PA_TOKEN")
     if(ghpt!="") {
+        git_config_set("user.name", "Gitlab CI bot")
+        git_config_set("user.email", "test@email.com")
+        git_status()
         git_branch_checkout("test")
         git_add("raw_data")
-        git_signature("Gitlab CI Bot", "test@test")
+        git_status()
+        cat("\nTrying to commit\n")
         git_commit("Update sam and rc data", author = "Gitlab CI bot")
         git_remote_add(glue::glue("https://covid19-ci:{ght}@github.com/mpiktas/covid19lt.git"), "github")
         git_push(remote = "github")
