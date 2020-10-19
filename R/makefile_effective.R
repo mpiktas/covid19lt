4d3bd3dded704b281959ad71b7adf473341f1b59
Vaidotas Zemlys <zemlys@gmail.com>
2020-10-19 16:48:48 +0300

    Do effective R calculation makefile

---
+++ R/makefile_effective.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(lubridate)
+library(testthat)
+library(gert)
+library(EpiEstim)
+
+source("R/effective.R")
+
+modf <- git_status() %>% .$file %>% find_root %>% unique
+if(!("data" %in% modf)) {
+    cat("\nNo new data, not pushing anything\n")
+} else {
+    cat("\nSending the new data downstream\n")
+    ghpt <- Sys.getenv("GITHUB_PA_TOKEN")
+    if(ghpt!="") {
+        cat("\nTrying to set signature\n")
+        git_config_set("user.name", "Gitlab CI bot")
+        git_config_set("user.email", "test@email.com")
+        cat("\nCurrent git status\n")
+        print(git_status())
+        git_add("raw_data")
+        git_add("data")
+        cat("\nTrying to commit\n")
+        git_commit("Update data.gov.lt data")
+        git_remote_add(glue::glue("https://covid19-ci:{ghpt}@github.com/mpiktas/covid19lt.git"), "github")
+        git_push(remote = "github")
+    } else {
+        cat("\nGithub token not found, relying on local git configuration\n")
+        git_add("raw_data")
+        git_add("data")
+        git_commit("Update data.gov.lt data")
+        git_push()
+    }
+}
+
+
