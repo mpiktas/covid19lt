847b311e44719b7ef8ea2f324dcdb74ec986a574
Vaidotas Zemlys <zemlys@gmail.com>
2020-11-10 14:48:07 +0200

    Update only part of the page

---
+++ R/makefile_part_site.R
@@ -0,0 +1,30 @@
+library(gert)
+library(dplyr)
+library(lubridate)
+
+rmarkdown::render_site("index.Rmd")
+rmarkdown::render_site("stats.Rmd")
+rmarkdown::render_site("hospital.Rmd")
+rmarkdown::render_site("Re.Rmd")
+
+cat("\nSending the site downstream\n")
+ghpt <- Sys.getenv("GITHUB_PA_TOKEN")
+if(ghpt!="") {
+    cat("\nTrying to set signature\n")
+    git_config_set("user.name", "Gitlab CI bot")
+    git_config_set("user.email", "test@email.com")
+    cat("\nCurrent git status\n")
+    print(git_status())
+    print(git_info())
+    git_branch_checkout("master")
+    git_add("docs")
+    cat("\nTrying to commit\n")
+    git_commit("Update the site")
+    git_remote_add(glue::glue("https://covid19-ci:{ghpt}@github.com/mpiktas/covid19lt.git"), "github")
+    git_push(remote = "github")
+} else {
+    cat("\nGithub token not found, relying on local git configuration\n")
+    git_add("docs")
+    git_commit("Update the index, stats, hospital and Re pages")
+    git_push()
+}
