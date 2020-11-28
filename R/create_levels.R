0fdec66ce87f674e1f48c7e3cb932c1e0633888d
Vaidotas Zemlys <zemlys@gmail.com>
2020-11-28 18:10:06 +0200

    Write out all the levels

--- R/create_levels.R
+++ R/create_levels.R
@@ -6,6 +6,18 @@ cs <- read.csv("data/lt-covid19-cases.csv") %>% mutate(day = ymd(day))
 ii <- read.csv("data/lt-covid19-individual.csv") %>% mutate(day = ymd(day))
 iii <- ii %>% group_by(day, administrative_level_3) %>% summarise(imported_daily = sum(imported == "Taip"))
 
+cvh <- read.csv("data/lt-covid19-hospitalized.csv") %>% mutate(day = ymd(day))
+vlk <- read.csv("raw_data/vlk_historical.csv") %>% mutate(day = ymd(day))
+
+hosp0 <- vlk %>% full_join(cvh %>% rename(vent = ventilated))
+hosp1 <- hosp %>% mutate(hospitalized = ifelse(is.na(hospitalized), total, hospitalized),
+                         icu = ifelse(is.na(icu), intensive, icu),
+                         ventilated = ifelse(is.na(ventilated), vent, ventilated),
+                         oxygen = ifelse(is.na(oxygen), oxygen_mask, oxygen)) %>%
+    select(day, hospitalized, icu, ventilated, oxygen) %>% arrange(day)
+
+
+
 fixNA <- function(x, fix = 0) {
     x[is.na(x)] <- fix
     x
@@ -25,7 +37,7 @@ oo <- cs %>%   filter(administrative_level_3!= "Lithuania") %>%select(-municipal
 
 sum(abs(cs %>% filter(administrative_level_3 == "Lithuania") %>% select(-administrative_level_3, - municipality_code, - day) - oo %>% select(-day)))
 
-lvl3 <- cs %>% select(-municipality_code) %>% filter(administrative_level_3 != "Lithuania") %>%
+lvl3 <- cs  %>% filter(administrative_level_3 != "Lithuania") %>%
     left_join(tt %>% select(-municipality_code)) %>% left_join(adm %>% select(-municipality_name)) %>%
     left_join(iii) %>%
     mutate(administrative_level_2 = ifelse(is.na(administrative_level_2), "Unknown",administrative_level_2)) %>%
@@ -40,7 +52,7 @@ lvl3 <- cs %>% select(-municipality_code) %>% filter(administrative_level_3 != "
 
 
 
-lvl31 <- lvl3 %>% select(day, administrative_level_2, administrative_level_3,
+lvl31 <- lvl3 %>% select(day, administrative_level_2, administrative_level_3, municipality_code,
                     confirmed = confirmed_cases_cumulative, tests = cumulative_tests,
                     deaths = deaths_cumulative, other_deaths = other_deaths_cumulative,
                     recovered = recovered_cases_cumulative, active = active_cases,
@@ -54,5 +66,16 @@ lvl2 <- lvl31 %>% select(-administrative_level_3) %>%
     group_by(day, administrative_level_2) %>% summarise_all(sum)
 
 lvl1 <- lvl2 %>% select(-administrative_level_2) %>% summarise_all(sum)
+
+lvl11 <- lvl1 %>% left_join(hosp1) %>%
+    mutate(hospitalized = fixNA(hospitalized),
+           icu = fixNA(icu),
+           ventilated = fixNA(ventilated),
+           oxygen = fixNA(oxygen))
+
+
+lvl31 %>% arrange(day, municipality_code) %>% select(-municipality_code) %>%  write.csv("data/lt-covid19-level3.csv", row.names = FALSE)
+lvl2 %>% write.csv("data/lt-covid19-level2.csv", row.names = FALSE)
+lvl11 %>% write.csv("data/lt-covid19-country.csv", row.names = FALSE)
 
 
