1da2123514f25de90e08a5965e4c8d0f0677f4cd
Vaidotas Zemlys <zemlys@gmail.com>
2020-11-29 22:46:27 +0200

    Do all the regions

--- R/render_regions.R
+++ R/render_regions.R
@@ -1,9 +1,10 @@
 library(dplyr)
 library(rmarkdown)
 
-regs <- read.csv("data/lt-covid19-individual.csv") %>% .$administrative_level_3 %>% unique %>% sort
+lvl3 <- read.csv("data/lt-covid19-level3.csv") %>% filter(administrative_level_3!= "Unknown")
 
-regs <- regs[regs!=""]
+regs <- c(lvl3$administrative_level_3 %>% unique %>% sort,lvl3$administrative_level_2 %>% unique %>% sort, "Lietuva")
+
 regs_n <- paste0(gsub("[.]","",gsub(" ", "_",regs)),".html")
 
 out <- mapply(function(r, f) try(rmarkdown::render("R/region.Rmd", output_dir = 'docs/regions/', output_file = f, params = list(region = r))),
