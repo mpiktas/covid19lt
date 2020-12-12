52adca0bb3ea409ea9eef34e943c9b354b0f849e
Vaidotas Zemlys <zemlys@gmail.com>
2020-12-12 22:05:49 +0200

    Add the test statistics

--- R/create_levels.R
+++ R/create_levels.R
@@ -84,6 +84,7 @@ add_stats <- function(dt) {
         mutate(cases_sum7 = rollsum(confirmed_daily, 7, fill = NA, align = "right"),
                cases_sum14 = rollsum(confirmed_daily, 14, fill = NA, align = "right"),
                test_sum7 = rollsum(tests_daily, 7, fill = NA, align = "right"),
+               test_sum14 = rollsum(tests_daily, 14, fill = NA, align = "right"),
                deaths_sum14 = rollsum(deaths_daily, 14, fill = NA, align = "right"),
                other_deaths_sum14 = rollsum(other_deaths_daily, 14, fill = NA, align = "right"),
                deaths_sum7 = rollsum(deaths_daily, 7, fill = NA, align = "right"),
@@ -97,6 +98,7 @@ add_stats <- function(dt) {
                deaths_100k = deaths_sum14/population*100000,
                other_deaths_100k = other_deaths_sum14/population*100000,
                all_deaths_100k = (deaths_sum14+other_deaths_sum14)/population*100000,
+               tests_100k = test_sum14/population*100000,
                confirmed_growth_weekly = round(100*(cases_sum7/lag(cases_sum7,7)-1),2),
                tpn_growth_weekly = round(100*(tpn_sum7/lag(tpn_sum7,7)-1),2),
                tpr_confirmed_diff_weekly = tpr_confirmed-lag(tpr_confirmed, 7),
@@ -105,7 +107,8 @@ add_stats <- function(dt) {
                tpn_100k_growth_weekly=round(100*(tpn_100k/lag(tpn_100k,7) - 1),2),
                deaths_growth_weekly=round(100*(deaths_sum7/lag(deaths_sum7,7) - 1),2),
                other_deaths_growth_weekly=round(100*(other_deaths_sum7/lag(other_deaths_sum7,7) - 1),2),
-               all_deaths_growth_weekly=round(100*((deaths_sum7+other_deaths_sum7)/lag(deaths_sum7+other_deaths_sum7,7) - 1),2)
+               all_deaths_growth_weekly=round(100*((deaths_sum7+other_deaths_sum7)/lag(deaths_sum7+other_deaths_sum7,7) - 1),2),
+               test_growth_weekly = round(100*(test_sum7/lag(test_sum7,7)-1),2)
         ) %>% select(-(confirmed_daily:tpn_sum14)) %>% ungroup
 }
 
