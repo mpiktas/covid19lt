fef035528919d247ec3cc312c0cd02e3d01c7e36
Vaidotas Zemlys <zemlys@gmail.com>
2020-12-26 18:02:27 +0200

    Move additional downloads into datagov makefile

--- R/makefile_datagov.R
+++ R/makefile_datagov.R
@@ -3,7 +3,17 @@ library(lubridate)
 library(testthat)
 library(gert)
 
-source("R/download_datagov.R")
+
+cat("\n=======Downloading tests================\n")
+try(source('R/download_osp_tests.R'))
+cat("\n=======Downloading cases================\n")
+try(source('R/download_osp_cases.R'))
+cat("\n=======Downloading age distribution data================\n")
+try(source("R/download_osp_agedist.R"))
+cat("\n=======Download datagov data================\n")
+try(source("R/download_datagov.R"))
+cat("\n=======Creating levels data================\n")
+try(source('R/create_levels.R'))
 
 
 find_root <- function(x) {
