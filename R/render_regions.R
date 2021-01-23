b5e48ec1b7c944fcbc8639b5a80495f7c1eaf503
Vaidotas Zemlys <zemlys@gmail.com>
2021-01-23 18:11:08 +0200

    Try to switch the rendering order

--- R/render_regions.R
+++ R/render_regions.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(rmarkdown)
 
+rmarkdown::render("R/regions_index.Rmd", output_dir = 'docs/', output_file = "savivaldybes.html")
+
+
 lvl3 <- read.csv("data/lt-covid19-level3.csv") %>% filter(administrative_level_3!= "Unknown")
 
 regs <- c(lvl3$administrative_level_3 %>% unique %>% sort,lvl3$administrative_level_2 %>% unique %>% sort, "Lietuva")
@@ -10,4 +13,3 @@ regs_n <- paste0(gsub("[.]","",gsub(" ", "_",regs)),".html")
 out <- mapply(function(r, f) try(rmarkdown::render("R/region.Rmd", output_dir = 'docs/regions/', output_file = f, params = list(region = r))),
               regs, regs_n, SIMPLIFY  = FALSE)
 
-rmarkdown::render("R/regions_index.Rmd", output_dir = 'docs/', output_file = "savivaldybes.html")
