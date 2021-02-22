8e6edbc0ef97f16be76aaf0a1e0feba6652c7de9
Vaidotas Zemlys <zemlys@gmail.com>
2021-02-22 13:48:04 +0200

    Fix apple mobility data

--- R/download_apple_mobility.R
+++ R/download_apple_mobility.R
@@ -5,14 +5,13 @@ library(glue)
 
 curdate<- ymd(Sys.Date())
 
-dd <- data.frame(date = ymd("2021-02-15")+days(0:20), hotfix =18+0:20)
-
+dd <- data.frame(date = ymd("2021-02-18")+days(0:20), hotfix =7+0:20)
 
 lap <- lapply(curdate - days(0:2), function(curd) {
     cdd <- dd %>% filter(date == curd)
     cd <- as.character(cdd$date)
     hf <- as.character(cdd$hotfix)
-    lnk <- glue::glue("https://covid19-static.cdn-apple.com/covid19-mobility-data/2101HotfixDev{hf}/v3/en-us/applemobilitytrends-{cd}.csv")
+    lnk <- glue::glue("https://covid19-static.cdn-apple.com/covid19-mobility-data/2102HotfixDev{hf}/v3/en-us/applemobilitytrends-{cd}.csv")
     print(lnk)
     try(read.csv(lnk))
 })
