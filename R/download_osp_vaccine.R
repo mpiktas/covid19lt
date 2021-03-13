ec91bfb77d093e84b87c06a2b7704c5893803a79
Vaidotas Zemlys <zemlys@gmail.com>
2021-03-14 00:10:54 +0200

    Add vaccine delivery analysis

--- R/download_osp_vaccine.R
+++ R/download_osp_vaccine.R
@@ -65,22 +65,6 @@ osp1 <- lapply(alls, function(l)fix_esridate(rawToChar(l$content))) %>% bind_row
 osp1 %>% arrange(date, municipality_code) %>% write.csv("raw_data/osp/osp_covid19_vaccine_supply.csv", row.names = FALSE)
 
 osp2 <- osp1 %>% mutate(day = ymd(date)) %>% filter(municipality_name == "Lietuva", dose_number == "Pirma dozė") %>%
-    filter(!is.na(vaccines_arrived) | !is.na(vaccines_allocated)) %>% select(day, vaccine_name, vaccines_arrived, vaccines_allocated)
+    filter(!is.na(vaccines_arrived) | !is.na(vaccines_allocated)) %>% select(day, vaccine_name, vaccines_arrived, vaccines_allocated) %>% arrange(day)
 
-adm <- read.csv("raw_data/administrative_levels.csv")
-
-osp3 <- osp2 %>% inner_join(adm)
-
-if(nrow(osp3) == nrow(osp2)) {
-    osp4 <- osp3 %>%select(day, municipality_code, administrative_level_2,administrative_level_3,
-                           dose_number,  vaccinated = vaccinated_cumulative, vaccinated_daily = vaccinated
-    )
-    dosed <- data.frame(dose_number=c("Pirma dozė", "Antra dozė"), dose = c(1,2))
-    osp5 <- osp4 %>% inner_join(dosed) %>% select(-dose_number)
-    osp6 <- osp5 %>% pivot_wider(id_cols = day:administrative_level_3, names_from = "dose",
-                                 values_from  = vaccinated:vaccinated_daily) %>% arrange(day, municipality_code)
-
-    osp6 %>%  write.csv("data/lt-covid19-vaccinated.csv", row.names = FALSE)
-
-
-}
+osp2 %>% write.csv("data/lt-covid19-vaccine-deliveries.csv", row.names  = FALSE)
