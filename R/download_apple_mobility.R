28fdfbf0cdddc21eb01c9554aa7d208f15e0f9da
Vaidotas Zemlys <zemlys@gmail.com>
2022-01-05 09:10:01 +0200

    Update Apple mobility data

--- R/download_apple_mobility.R
+++ R/download_apple_mobility.R
@@ -5,7 +5,7 @@ library(glue)
 
 curdate <- ymd(Sys.Date())
 
-dd <- data.frame(date = ymd("2021-10-04") + days(0:20), hotfix = 15 + 0:20)
+dd <- data.frame(date = ymd("2022-01-01") + days(0:20), hotfix = 31 + 0:20)
 # begin nolint
 # https://covid19-static.cdn-apple.com/covid19-mobility-data/2105HotfixDev8/v3/en-us/applemobilitytrends-2021-04-02.csv
 # https://covid19-static.cdn-apple.com/covid19-mobility-data/2104HotfixDev11/v3/en-us/applemobilitytrends-2021-03-21.csv
@@ -20,13 +20,14 @@ dd <- data.frame(date = ymd("2021-10-04") + days(0:20), hotfix = 15 + 0:20)
 # https://covid19-static.cdn-apple.com/covid19-mobility-data/2115HotfixDev10/v3/en-us/applemobilitytrends-2021-08-22.csv
 # https://covid19-static.cdn-apple.com/covid19-mobility-data/2116HotfixDev8/v3/en-us/applemobilitytrends-2021-09-02.csv
 # https://covid19-static.cdn-apple.com/covid19-mobility-data/2118HotfixDev19/v3/en-us/applemobilitytrends-2021-10-08.csv
+# https://covid19-static.cdn-apple.com/covid19-mobility-data/2204HotfixDev33/v3/en-us/applemobilitytrends-2022-01-03.csv
 # end nolint
 
 lap <- lapply(curdate - days(0:2), function(curd) {
   cdd <- dd %>% filter(date == curd)
   cd <- as.character(cdd$date)
   hf <- as.character(cdd$hotfix)
-  lnk <- glue::glue("https://covid19-static.cdn-apple.com/covid19-mobility-data/2118HotfixDev{hf}/v3/en-us/applemobilitytrends-{cd}.csv") # nolint
+  lnk <- glue::glue("https://covid19-static.cdn-apple.com/covid19-mobility-data/2204HotfixDev{hf}/v3/en-us/applemobilitytrends-{cd}.csv") # nolint
   print(lnk)
   try(read.csv(lnk))
 })
