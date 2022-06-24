a7d342b253c82725f65451c160c4ea3026311541
Vaidotas Zemlys <zemlys@gmail.com>
2022-06-24 16:57:45 +0300

    Fix age dist data to account for new infection data

--- R/download_osp_agedist.R
+++ R/download_osp_agedist.R
@@ -9,25 +9,29 @@ library(tidyr)
 
 source("R/functions.R")
 
-httr::set_config(config(ssl_verifypeer = 0L, ssl_verifyhost = 0L))
+if (FALSE) {
+  httr::set_config(config(ssl_verifypeer = 0L, ssl_verifyhost = 0L))
 
-posp <- tryget("https://services3.arcgis.com/MF53hRPmwfLccHCj/arcgis/rest/services/Agreguoti_COVID19_atvejai_ir_mirtys/FeatureServer/0/query?where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=date+desc&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token=") # nolint
+  posp <- tryget("https://services3.arcgis.com/MF53hRPmwfLccHCj/arcgis/rest/services/Agreguoti_COVID19_atvejai_ir_mirtys/FeatureServer/0/query?where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=date+desc&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token=") # nolint
 
-posp1 <- fix_esridate(rawToChar(posp$content))
-posp2 <- posp1 %>% mutate(day = ymd(date))
+  posp1 <- fix_esridate(rawToChar(posp$content))
+  posp2 <- posp1 %>% mutate(day = ymd(date))
 
-posp3 <- posp2 %>% filter(day == max(day))
+  posp3 <- posp2 %>% filter(day == max(day))
+}
 
 
-osp0 <- read.csv("https://opendata.arcgis.com/datasets/ba35de03e111430f88a86f7d1f351de6_0.csv") # nolint
+osp1 <- read.csv("https://opendata.arcgis.com/datasets/ba35de03e111430f88a86f7d1f351de6_0.csv") # nolint
 
-dd <- ymd(unique(osp0$date))
-ld <- ymd(posp3$date %>% unique())
-if (!(ld %in% dd)) {
-  warning("Last day not present, adding a new day from another source")
-  osp1 <- bind_rows(osp0, posp3 %>% select(-day))
-} else {
-  osp1 <- osp0
+if (FALSE) {
+  dd <- ymd(ymd_hms(unique(osp0$date)))
+  ld <- ymd(posp3$date %>% unique())
+  if (!(ld %in% dd)) {
+    warning("Last day not present, adding a new day from another source")
+    osp1 <- bind_rows(osp0, posp3 %>% select(-day))
+  } else {
+    osp1 <- osp0
+  }
 }
 
 osp1 %>%
@@ -35,7 +39,7 @@ osp1 %>%
   select(-object_id) %>%
   write.csv("raw_data/osp/osp_covid19_agedist.csv", row.names = FALSE)
 
-osp2 <- osp1 %>% mutate(day = ymd(date))
+osp2 <- osp1 %>% mutate(day = ymd(ymd_hms(date)))
 
 adm <- read.csv("raw_data/administrative_levels.csv") %>%
   rbind(data.frame(
@@ -54,7 +58,11 @@ if (nrow(osp3) == nrow(osp2)) {
   osp4 <- osp3 %>%
     select(day, administrative_level_2, administrative_level_3,
       age = age_gr, sex = sex,
-      confirmed_daily = new_cases,
+      confirmed_daily = incidence,
+      infection_1_daily = infection_1,
+      infection_2_daily = infection_2,
+      infection_3_daily = infection_3,
+      infection_4_daily = infection_4,
       deaths_population_daily = deaths_all,
       deaths_1_daily = deaths_cov1,
       deaths_2_daily = deaths_cov2,
